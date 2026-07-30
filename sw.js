dd823edbf50cf5cba9a84da7da11f07b1a06cca1
choioioi76-blip <choioioi76@gmail.com>
2026-07-30 17:13:05 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,25 @@
 const CACHE_NAME = 'goaltrack-v1';
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json'
+  './',
+  './index.html',
+  './manifest.json',
+  './MHRL%20icon.png'
 ];
 
+// 서비스 워커 설치 및 파일 캐싱
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
   );
 });
 
+// 오프라인 상태 및 네트워크 요청 처리
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
+    })
   );
 });
